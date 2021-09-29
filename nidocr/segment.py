5d54ee6d5278b2b43c797c1356e218ef0a48eca3
rezwanh001 <rezwanh001@gmail.com>
2021-09-29 22:21:21 +0600

    nidocr: unstable

---
+++ nidocr/segment.py
@@ -0,0 +1,84 @@
+#-*- coding: utf-8 -*-
+"""
+@author:MD.Nazmuddoha Ansary
+"""
+from __future__ import print_function
+#-------------------------
+# imports
+#-------------------------
+import tensorflow as tf
+import os
+import numpy as np
+import cv2 
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
+import segmentation_models as sm
+from .utils import *
+import matplotlib.pyplot as plt
+#-------------------------
+# model
+#------------------------
+class Extractor(object):
+    def __init__(self,
+                model_weights,
+                img_dim=(256,256,3),
+                data_channel=1,
+                num_classes=2,
+                labels=["nid","smart"],
+                backbone='densenet121'):
+        # Extractor with unet
+        self.img_dim=img_dim
+        self.data_channel=data_channel
+        self.backbone=backbone
+        self.num_classes=num_classes
+        self.labels=labels
+        
+        unet=sm.Unet(self.backbone,input_shape=self.img_dim,encoder_weights=None,classes=self.data_channel)
+        inp  =unet.input
+        # class
+        label=unet.get_layer(name="relu").output
+        label = tf.keras.layers.GlobalAveragePooling2D()(label)
+        label=tf.keras.layers.Dense(self.num_classes,activation="softmax",name="label")(label)
+        # mask
+        mask=unet.output
+        self.model=tf.keras.Model(inputs=inp,outputs=[label,mask])
+        # load weights        
+        self.model.load_weights(model_weights)
+    
+    def process(self,img):
+        # process if path is provided
+        if type(img)==str:
+            img=cv2.imread(img)
+        org=np.copy(img)
+        # dims
+        h,w,d=img.shape
+        # process
+        img=cv2.resize(img,(self.img_dim[1],self.img_dim[0]))
+        img=img/255.0
+        data=np.expand_dims(img,axis=0)
+        # predict
+        pred=self.model.predict(data)
+        card_type=self.labels[np.argmax(pred[0][0])]
+        card_map =pred[1][0]
+        card_map=cv2.resize(card_map,(w,h))
+        # find region
+        y_min,y_max,x_min,x_max=locateData(card_map,0)
+        org=org[y_min:y_max,x_min:x_max]
+        card_map=card_map[y_min:y_max,x_min:x_max]
+        
+         # find max contour (4 point)
+        contours, hierarchy = cv2.findContours(card_map.astype("uint8"), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        cmax = max(contours, key = cv2.contourArea)
+        hull=cv2.convexHull(cmax)
+        for i in range(10):
+            epsilon = (i+1)*0.01*cv2.arcLength(hull,True)
+            approx = cv2.approxPolyDP(hull,epsilon,True)
+            if approx.shape[-1]==4:
+                break
+            
+        approx=np.reshape(approx,(approx.shape[0],approx.shape[-1]))
+        approx=approx.astype("float32")
+        form=np.array([approx[0],approx[3],approx[2],approx[1]])
+        # warp
+        org=four_point_transform(org,form)
+        return card_type,org
+
