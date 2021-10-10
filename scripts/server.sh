097216717bfd55dcd7f9be8c53c4191b4fe97242
silentassasin0111 <silentassasin0111@gmail.com>
2021-10-10 18:35:55 +0600

    dataLib: CSeg: Large

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#save_path="/home/apsisdev/ansary/DATASETS/APSIS/NID/"
-save_path="/media/ansary/DriveData/Work/APSIS/datasets/NID/"
+save_path="/home/apsisdev/ansary/DATASETS/APSIS/NID/"
+#save_path="/media/ansary/DriveData/Work/APSIS/datasets/NID/"
 #-----------------------------------------------------------------------------------------------
 src_path="${save_path}source/"
 card_path="${save_path}cards/"
@@ -10,7 +10,7 @@ rec_path="${save_path}recog/"
 det_path="${save_path}detect/"
 proc_path="${save_path}processed/"
 #------------------------------------------card------------------------------------------------------
-python datagen_card.py $src_path $save_path --num_data 10
+#python datagen_card.py $src_path $save_path --num_data 50000
 #----------------------------------------------------------------------------------------------------
 #------------------------------------------class------------------------------------------------------
 #python datagen_class.py $src_path $card_path $save_path 
